381ae118d67a6ed907e40e8c0e709d4e7ca5c641
David <davidstankoviic@gmail.com>
2021-06-02 18:26:56 +0200

    Add: Initialize app, Landing Page, Prepare libraries for future use

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontSize: {
+      'uni': '8vw'
+    },
+    extend: {
+      colors: {
+        background_mood: {
+          dark: "#2b1055",
+          light: "#7597de"
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
