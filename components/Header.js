a454ce685b47ac79996830bd01161e86fb79752f
David <davidstankoviic@gmail.com>
2021-06-03 20:48:29 +0200

    Add: Header Component

---
+++ components/Header.js
@@ -0,0 +1,27 @@
+function Header() {
+    return (
+        <header className="absolute z-50 w-full px-16 py-16">
+            <div className='flex items-center justify-between'>
+                <div className='font-extrabold tracking-wider text-white'>HOWL</div>
+                <nav>
+                    <ul className="flex">
+                        <li className="list-none mx-16">
+                            <a href='/' className="text-white no-underline uppercase">discover</a>
+                        </li>
+                        <li className="list-none mx-16">
+                            <a href='/' className="text-white no-underline uppercase">favorites</a>
+                        </li>
+                        <li className="list-none mx-16">
+                            <a href='/' className="text-white no-underline uppercase">streaming</a>
+                        </li>
+                        <li className="list-none mx-16">
+                            <a href='/' className="text-white no-underline uppercase">profile</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header
